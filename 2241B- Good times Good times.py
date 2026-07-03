7be77af324f8693a8202ecb3dc6596385c2d43a0
Arein64 <areinparmar64@gmail.com>
2026-07-03 14:07:08 +0530

    Day 13

--- 2241B- Good times Good times.py
+++ 2241B- Good times Good times.py
@@ -3,18 +3,12 @@ n = int(input())
 int_max = 10**9
 
 ans = []
-
-def good_numbercheck(n):
-    return True if len(set(str(n)))<=2 else False
          
 for _ in range(n):
     x = int(input())
-    ch = False
-    arr = []
-    for j in range(2,int_max):
-        if good_numbercheck(j) and good_numbercheck(j*x):
-            ans.append(j)
-            break
+    l = len(str(x))
+    y = 10**l + 1
+    ans.append(y)
 
 for i in ans:
     print(i)
