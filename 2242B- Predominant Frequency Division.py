4e3d077a9982f816520bd01a8ab459410d3e2d35
Arein64 <areinparmar64@gmail.com>
2026-07-11 23:51:31 +0530

    Day 21

--- 2242B- Predominant Frequency Division.py
+++ 2242B- Predominant Frequency Division.py
@@ -1,47 +1,35 @@
-n = int(input())
+n=int(input())
 
-ans = []
-
+ans=[]
 for _ in range(n):
-    k = int(input())
-    m = False
-    arr = list(map(int,input().split()))
-    p1,p2,p3 = False, False, False
-    l=0
-    c1,c2,c3 = 0,0,0
-    while l<k:
-        if arr[l]==1:
-            c1+=1
-        elif arr[l]==2:
-            c2+=1
-        elif arr[l]==3:
-            c3+=1
-        l+=1
+    k=int(input())
+    arr=list(map(int,input().split()))
+    c1,c2= 0,0
+    mn=float('inf')
+    ok=False
 
-        if c1 >= c2+c3:
-            p1= True
-            break
-    
-    c1,c2,c3 = 0,0,0
-    while l<k:
-        if arr[l]==1:
+    for i in range(k-1):
+        if arr[i]==1:
             c1+=1
-        elif arr[l]==2:
-            c2+=1
-        elif arr[l]==3:
-            c3+=1
-        l+=1
+        else:
+            c1-=1
 
-        if c3 <= c1+c2:
-            p2= True
+        if arr[i]==3:
+            c2-=1
+        else:
+            c2+=1
+
+        if c2>=mn:
+            ok=True
             break
-    
-    if l<k:
-        p3 = True
-        
-    if p1 and p2 and p3:
+
+        if c1>=0:
+            mn=min(mn,c2)
+
+    if ok:
         ans.append("YES")
     else:
         ans.append("NO")
+
 for i in ans:
     print(i)
